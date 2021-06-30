70155b7aa3a46f2cea565bbd9d6ff9d34e575c9e
Viwat Te <31836504+teviw000@users.noreply.github.com>
2021-06-30 14:57:42 -0500

    Add files via upload
    Saw the difference between useRef and useState for managing input forms. Ref is good for a simple singular input while State is good for things that get changed a lot of reset to default input.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,7 +1,8 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
 
   const nameInputChangeHander = (event) => {
     setEnteredName(event.target.value);
@@ -9,14 +10,26 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
     console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    // nameInputRef.current.value='';
+    setEnteredName("");
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHander} />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHander}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
