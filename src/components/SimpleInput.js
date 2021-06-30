1fc4c504226692fa8a77591517d9736c9c3b565a
Viwat Te <31836504+teviw000@users.noreply.github.com>
2021-06-30 15:16:07 -0500

    Add files via upload
    Added validation for the input field, showing error message and making class type for form dynamic based on user input.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHander = (event) => {
     setEnteredName(event.target.value);
@@ -11,17 +12,29 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    if (enteredName.trim() == "") {
+      //trrim removes white space
+      setEnteredNameIsValid(false);
+      return; //exits if no input and doesnt send in request to server
+    }
+
+    setEnteredNameIsValid(true);
+
     console.log(enteredName);
 
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
-    // nameInputRef.current.value='';
+    // nameInputRef.current.value=''; => not ideal, dont manipulate the dom
     setEnteredName("");
   };
 
+  const nameInputClasses = enteredNameIsValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -30,6 +43,9 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHander}
           value={enteredName}
         />
+        {!enteredNameIsValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
